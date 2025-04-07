c5c5aa7e266c4297f272fb9e3be56a08a9955549
santokan <houska.peter@gmail.com>
2025-04-07 23:13:10 +0200

    add: counter and reset

--- main.go
+++ main.go
@@ -1,18 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func handlerReady(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Hits: " + fmt.Sprintf("%d", cfg.fileserverHits.Load())))
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Metrics reset"))
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
 
 func main() {
 	const (
 		filePathRoot = "."
 		port         = "8080"
 	)
+
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.Handle("/healthz", http.HandlerFunc(healthCheck))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("/healthz", handlerReady)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -21,13 +66,4 @@ func main() {
 
 	log.Printf("Serving files from '%s' on http://localhost:%s", filePathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte("OK"))
-	if err != nil {
-		log.Printf("Error writing response: %v", err)
-	}
 }
