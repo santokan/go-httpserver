1286f7cae3fa99d5a16b22e1df48c7aefbdadaf4
santokan <houska.peter@gmail.com>
2025-04-09 00:44:08 +0200

    add: chirpValidator with length and profane words

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -56,6 +59,68 @@ func handlerReady(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, `{"error": "Invalid request method"}`, http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestBody struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		log.Printf("Error decoding JSON: %v", err)
+		http.Error(w, `{"error": "Something went wrong"}`, http.StatusInternalServerError)
+		return
+	}
+
+	if len(requestBody.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error": "Chirp is too long"}`))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	cleanedBody := cleanChirp(requestBody.Body)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	responseBody := struct {
+		CleanedBody string `json:"cleaned_body"`
+	}{
+		CleanedBody: cleanedBody,
+	}
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(responseBody)
+	if err != nil {
+		log.Printf("Error encoding response JSON: %v", err)
+		// Attempt to send a generic error if encoding fails
+		http.Error(w, `{"error": "Something went wrong"}`, http.StatusInternalServerError)
+	}
+}
+
+// cleanChirp replaces profane words in a string with "****".
+func cleanChirp(body string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(body, " ")
+	cleanedWords := make([]string, len(words))
+
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+		isProfane := slices.Contains(profaneWords, lowerWord)
+		if isProfane {
+			cleanedWords[i] = "****"
+		} else {
+			cleanedWords[i] = word
+		}
+	}
+
+	return strings.Join(cleanedWords, " ")
+}
+
 func main() {
 	const (
 		filePathRoot = "."
@@ -69,6 +134,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReady)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handerMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
 
 	server := &http.Server{
 		Addr:    ":" + port,
